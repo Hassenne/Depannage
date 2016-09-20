f1bbc606c6e9e0192b4a7d524568d1a40d2b55ee
Hassenne Ben youssef <attig.benyoussefhassenne@gmail.com>
2016-09-20 10:26:08 +0200

    demo-1

--- js/custom.js
+++ js/custom.js
@@ -11,13 +11,13 @@ $(document).ready(function(){
         if($(window).scrollTop() < 80 ) {
             
             $('.navbar').css ({
-               'margin-top': '-100px',
-                'opacity': '0'
+               'margin-top': '0px',
+                'opacity': '1'
                 
             });
             
             $('.navbar-default').css({
-                'background-color': 'rgba(59, 59 , 59, 0)'
+                'background-color': '#fff'
                 
             });
             
